0906dab64d52e4cd384def9ae4c85a9ab1aa29f2
lhern207 <lhern207@fiu.edu>
2019-01-04 10:18:20 -0500

    Commented out record creation bean

--- src/main/java/com/vega/springit/SpringitApplication.java
+++ src/main/java/com/vega/springit/SpringitApplication.java
@@ -23,7 +23,7 @@ public class SpringitApplication {
         SpringApplication.run(SpringitApplication.class, args);
     }
 
-    @Bean
+    //@Bean
     CommandLineRunner runner(LinkRepository linkRepository, CommentRepository commentRepository) {
         return args -> {
             Link link = new Link("Getting Started with Spring Boot 2", "https://therealdanvega.com/spring-boot-2");
@@ -37,8 +37,5 @@ public class SpringitApplication {
             System.out.println("===================================================");
         };
     }
-
-
-
 }
 
